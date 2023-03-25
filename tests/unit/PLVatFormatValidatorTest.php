3f8ab163b0a34204f590dffafd395effdbe3a85a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-25 19:23:06 +0400

    [CVFV-15] setup.

--- tests/unit/PLVatFormatValidatorTest.php
+++ tests/unit/PLVatFormatValidatorTest.php
@@ -1,22 +1,21 @@
 <?php
 
-// TODO: set namespace
-namespace unit;
+namespace rocketfellows\PLVatFormatValidator\tests\unit;
 
-class CountryVatFormatValidatorTest extends TestCase
+use PHPUnit\Framework\TestCase;
+
+class PLVatFormatValidatorTest extends TestCase
 {
     /**
-     * TODO: set type
-     * @var
+     * @var PLVatFormatValidator
      */
     private $validator;
 
-    /**
-     * TODO: setup validator for test
-     */
     protected function setUp(): void
     {
-        $this->validator = new CountryVatFormatValidator();
+        parent::setUp();
+
+        $this->validator = new PLVatFormatValidator();
     }
 
     /**
